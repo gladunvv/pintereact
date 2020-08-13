5f87aa899af64435fa7cae5c9a696a74886e2094
vladislav <bincha.1997@gmail.com>
2020-08-14 00:53:16 +1000

    feat: added new components

---
+++ src/components/ImageCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const ImageCard = ({ image, clickTag }) => {
+  const tags = image.tags.split(', ');
+  return (
+    <div className='max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl transition duration-500 cursor-pointer'>
+      <img src={image.webformatURL} alt='' className='w-full' />
+      <div className='px-6 py-4'>
+        <div className='font-bold text-purple-500 text-xl mb-2'>
+          Photo by{' '}
+          <a
+            href={`https://pixabay.com/ru/users/${image.user}-${image.user_id}/`}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hover:text-purple-700'
+          >
+            {image.user}
+          </a>
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong> {image.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong> {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong> {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className='px-6 py-4'>
+        {tags.map((tag, index) => (
+          <span
+            onClick={() => clickTag(tag)}
+            key={index}
+            className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 transition duration-700 hover:text-gray-900 hover:bg-gray-400'
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className='text-center text-6xl mt-3 font-mono'>
+      <h1>
+        Pintereact{' '}
+        <span aria-label='pin' role='img'>
+          📌
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
