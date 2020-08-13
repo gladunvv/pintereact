8eb408112b9879d02a1792f06fc31a20f0410d8b
vladislav <bincha.1997@gmail.com>
2020-08-14 01:11:16 +1000

    Updates

---
+++ precache-manifest.3fe7cc92632e01d09a2a93bb14b195f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83dc90935746b57eba3b53605291fc9",
+    "url": "/pintereact/index.html"
+  },
+  {
+    "revision": "4f236c9d9537a513e25b",
+    "url": "/pintereact/static/css/main.f96ddc9c.chunk.css"
+  },
+  {
+    "revision": "ca5938604fd338a5b2a9",
+    "url": "/pintereact/static/js/2.ffb51b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pintereact/static/js/2.ffb51b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f236c9d9537a513e25b",
+    "url": "/pintereact/static/js/main.0e447768.chunk.js"
+  },
+  {
+    "revision": "85568967321881abecbe",
+    "url": "/pintereact/static/js/runtime-main.5ffcaf7a.js"
+  }
+]);
